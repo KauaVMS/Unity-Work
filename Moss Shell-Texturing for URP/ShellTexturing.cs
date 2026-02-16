c772b235418294cb86cac43ed95961565fdd9cc3
Kauã V.M.S <kauvini0324@gmail.com>
2026-02-16 01:49:34 -0300

    Update ShellTexturing.cs

--- Moss Shell-Texturing for URP/ShellTexturing.cs
+++ Moss Shell-Texturing for URP/ShellTexturing.cs
@@ -4,26 +4,46 @@ using UnityEngine;
 
 public class ShellTexturing : MonoBehaviour
 {
-	public int Iterations;
+	// Determines the amount of shells to be produced
+	// Determina quantas camadas serão produzidas
+	public int Iterations = 10;
 
 	void Start()
 	{
+		// Defines a "mesh" variable containing the mesh to be rendered and a "mat" variable for the material
+		// Define uma variavel "mesh" contendo a malha que sera renderizada e uma variavel "mat" para o material
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		Material mat = GetComponent<MeshRenderer>().material;
+		
+		//For loop to instantiate (create) all shell GameObjects
+		//Estrutura de repetição For para instanciar (criar) todos os GameObjects de camadas
 		for (float i=0; i<1; i+=1f/Iterations)
 		{
+			// Creates a new GameObject
+			// Cria um novo GameObject
 			GameObject shell = new GameObject("Shell" + i.ToString());
-			
+
+			// Adds the mesh-related components
+			// Adiciona os componentes relacionados a malha
 			shell.AddComponent<MeshFilter>();
    			shell.AddComponent<MeshRenderer>();
-            
-   			shell.GetComponent<MeshFilter>().mesh = GetComponent<MeshFilter>().mesh;
-     			shell.GetComponent<MeshRenderer>().material = GetComponent<MeshRenderer>().material;
-     			shell.transform.SetParent(transform, false);	
-     			
-     			shell.GetComponent<MeshRenderer>().material.SetFloat("_Height", i);
+
+			// Sets up the mesh and materials
+			// Configura a malha e os materiais
+   			shell.GetComponent<MeshFilter>().mesh = mesh;
+     		shell.GetComponent<MeshRenderer>().material = mat;
+     		shell.transform.SetParent(transform, false);	
+
+			// Sets the height in the material to the instance's ID
+			// Define a altura no material como o ID da instancia
+     		shell.GetComponent<MeshRenderer>().material.SetFloat("_Height", i);
 		}
-		
+
+		// Destroys the original mesh, as the shells replace it
+		// Destroi a malha original, dado as camadas substituirem ele
 		Destroy(GetComponent<MeshFilter>());
 		Destroy(GetComponent<MeshRenderer>());
 	}
 }
+
 
